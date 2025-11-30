1f34e54a1d2f0fec2a9a72bd77590e79014a80e3
Mazen Essam <mazenessam2011@outlook.com>
2025-11-30 20:51:22 +0200

    Create new list view Trial 1

---
+++ components/CarList/CarList.tsx
@@ -0,0 +1,86 @@
+import { JSX } from "react";
+import AutotraderLogo from "../Logos/AutotraderLogo";
+import FacebookLogo from "../Logos/FacebookLogo";
+import KijijiLogo from "../Logos/KijijiLogo";
+import { CircleGauge } from "lucide-react";
+import Link from "next/link";
+export default function CarList({ carDetails }: { carDetails: any }) {
+  const logoMap: Record<string, JSX.Element> = {
+    Autotrader: <AutotraderLogo className="w-10" />,
+    Kijiji: <KijijiLogo className="w-10" />,
+    Marketplace: <FacebookLogo className="w-8" />,
+  };
+  const statusStyleMap: Record<string, { bg: string; border: string }> = {
+    Steal: {
+      bg: "bg-green-700",
+      border: "border-green-700",
+    },
+    Good: {
+      bg: "bg-green-400",
+      border: "border-green-400",
+    },
+    Fair: {
+      bg: "bg-yellow-500",
+      border: "border-yellow-500",
+    },
+    Pass: {
+      bg: "bg-red-500",
+      border: "border-red-500",
+    },
+  } as const;
+
+  return (
+    <div className=" h-40  flex bg-gray-200 rounded-lg shadow-md cursor-pointer hover:shadow-xl transition-shadow duration-300 ease-in-out relative ">
+      <div className="w-40 sm:w-60 md:w-3xs h-full overflow-hidden rounded-md ">
+        <img
+          src={carDetails.image_src}
+          alt={carDetails.title}
+          className="w-full object-cover h-full"
+        />
+      </div>
+      <div className="absolute top-2 left-2 opacity-50">
+        {logoMap[carDetails.Source]}
+      </div>
+      <div className="w-full h-full p-5  flex flex-col justify-between">
+        <div className="flex gap-2 flex-wrap ">
+          <p
+            className={`border border-solid border-gray-300 text-gray-200 bg-primary  
+             
+          shadow-md   px-2.5 py-0.5  rounded-md text-xs w-fit sm:text-base`}
+          >
+            CA${carDetails.price}
+          </p>
+          <div className="flex">
+            <p
+              className={` ${statusStyleMap[carDetails.status]?.bg}
+              text-white px-2.5 py-0.5 rounded-l-md`}
+            >
+              {carDetails.status}
+            </p>
+            <p
+              className={`border border-solid ${
+                statusStyleMap[carDetails.status]?.border
+              } 
+            flex items-center shadow-md text-gray-700 px-2.5 py-0.5  rounded-r-md text-xs w-fit sm:text-base`}
+            >
+              Est. value ~ CA${carDetails.estValue}
+            </p>
+          </div>
+        </div>
+        <div className="flex justify-between flex-wrap items-center  ">
+          <p className="text-black  overflow-ellipsis line-clamp-2  sm:line-clamp-1 font-bold text-base sm:text-lg pr-2.5">
+            {carDetails.title}
+          </p>
+        </div>
+
+        <p className="text-black overflow-ellipsis max-sm:hidden line-clamp-1 lg:line-clamp-2 text-sm">
+          {carDetails.description}
+        </p>
+        <p className="text-gray-500 flex items-center text-sm sm:text-base gap-2">
+          <CircleGauge className="w-4 h-4" />
+          {carDetails.odometer.toLocaleString()} Km
+        </p>
+      </div>
+    </div>
+  );
+}
--- components/Listings/Listings.tsx
+++ components/Listings/Listings.tsx
@@ -1,18 +1,20 @@
 import CarCard from "../CarCard/CarCard";
+import CarList from "../CarList/CarList";
 export default function Listings({ active }: { active: string }) {
   const carDetailsArray = [
     {
       id: 1,
       Source: "Autotrader",
-      title: "2020 Honda Civic Sedan 4dr Auto LX",
-      price: 25000,
+      title: "2020 Honda Civic Sedan 4dr Auto LX ",
+      price: 250000,
       location: "Toronto, ON",
       image_src:
         "https://www.hdcarwallpapers.com/thumbs/2020/2020_honda_civic_type_r_5k_4-t2.jpg",
       odometer: 15000,
-      estValue: 28000,
+      estValue: 280000,
       status: "Steal",
-      ad_link: "/listings/1",
+      ad_link:
+        "https://www.autotrader.ca/a/toyota/corolla/london/ontario/19_13230729_/?showcpo=ShowCpo&ncse=no&ursrc=boost&orup=1_15_953&pc=N6B%203B4&sprx=100&lstIdx=9&srtOrd=ScoringProfileBoostDesc&lstName=advancedSearch",
       description: `2015 Civic Sedan
         Automatic
         125K
@@ -34,7 +36,8 @@ export default function Listings({ active }: { active: string }) {
       odometer: 15000,
       estValue: 26000,
       status: "Good",
-      ad_link: "/listings/1",
+      ad_link:
+        "https://www.kijiji.ca/v-cars-vehicles/london/2015-honda-civic-sedan-4dr-auto-lx/1534567890",
       description: `2015 Civic Sedan
         Automatic
         125K
@@ -98,7 +101,7 @@ export default function Listings({ active }: { active: string }) {
           {active} Listings
         </p>
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-12">
+      {/* <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-12">
         <CarCard carDetails={carDetailsArray[0]} />
         <CarCard carDetails={carDetailsArray[1]} />
         <CarCard carDetails={carDetailsArray[2]} />
@@ -115,6 +118,24 @@ export default function Listings({ active }: { active: string }) {
         <CarCard carDetails={carDetailsArray[0]} />
         <CarCard carDetails={carDetailsArray[0]} />
         <CarCard carDetails={carDetailsArray[0]} />
+      </div> */}
+      <div className="grid grid-cols-1 gap-5">
+        <CarList carDetails={carDetailsArray[0]} />
+        <CarList carDetails={carDetailsArray[1]} />
+        <CarList carDetails={carDetailsArray[2]} />
+        <CarList carDetails={carDetailsArray[3]} />
+        <CarList carDetails={carDetailsArray[0]} />
+        <CarList carDetails={carDetailsArray[0]} />
+        <CarList carDetails={carDetailsArray[0]} />
+        <CarList carDetails={carDetailsArray[0]} />
+        <CarList carDetails={carDetailsArray[0]} />
+        <CarList carDetails={carDetailsArray[0]} />
+        <CarList carDetails={carDetailsArray[0]} />
+        <CarList carDetails={carDetailsArray[0]} />
+        <CarList carDetails={carDetailsArray[0]} />
+        <CarList carDetails={carDetailsArray[0]} />
+        <CarList carDetails={carDetailsArray[0]} />
+        <CarList carDetails={carDetailsArray[0]} />
       </div>
     </div>
   );
