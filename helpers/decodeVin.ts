a9101c4e3a8b6966de32136ae6ba15cffe2c7971
Mazen Essam <mazenessam2011@outlook.com>
2025-12-22 20:48:04 +0200

    test new vin api

--- helpers/decodeVin.ts
+++ helpers/decodeVin.ts
@@ -1,11 +1,15 @@
 export default async function decodeVin(vin: string) {
-  const url = `https://vpic.nhtsa.dot.gov/api/vehicles/decodevinvalues/${vin}?format=json`;
+  // const url = `https://vpic.nhtsa.dot.gov/api/vehicles/decodevinvalues/${vin}?format=json`;
+  const url = `https://api.marketcheck.com/v2/decode/car/neovin/${vin}/specs?api_key=${"fVHi4Jmee2osQkopWH3GW3s3ytaMK6bh"}`;
 
-  const res = await fetch(url);
+  const res = await fetch(url, {
+    method: "GET",
+    headers: { Accept: "application/json" },
+  });
   if (!res.ok) {
     throw new Error(`HTTP error ${res.status}`);
   }
 
   const data = await res.json();
-  return data.Results?.[0] ?? {};
+  return data;
 }
