a2ca44883ab4bb3f2b93ed80a730c0019666f904
Mazen Essam <mazenessam2011@outlook.com>
2026-01-07 00:19:03 +0200

    edit with sus v3

--- components/CarCard/CarCard.tsx
+++ components/CarCard/CarCard.tsx
@@ -106,9 +106,21 @@ export default function CarCard({
               Est. value ~ CA${formatNumber(carDetails.est_value)}
             </p>
           </div>
-          <div className="border border-gray-300 text-gray-200  shadow-md px-2.5 py-0.5 rounded-md cursor-default  w-fit sm:text-base text-sm  bg-red-600 ">
-            SUS
-          </div>
+          {carDetails.is_sus !== null ? (
+            <div
+              className={`border border-gray-300 text-gray-200  shadow-md px-2.5 py-0.5 rounded-md cursor-default  w-fit sm:text-base text-sm  ${
+                carDetails.is_sus ? "bg-red-600" : "bg-green-600"
+              }`}
+            >
+              {carDetails.is_sus ? "Suspicious" : "Safe"}
+            </div>
+          ) : (
+            <div
+              className={`border border-gray-300 text-gray-200  shadow-md px-2.5 py-0.5 rounded-md cursor-default  w-fit sm:text-base text-sm bg-gray-500 `}
+            >
+              Unknown
+            </div>
+          )}
         </div>
         <p className="text-black overflow-ellipsis line-clamp-2 text-sm">
           {carDetails.description}
