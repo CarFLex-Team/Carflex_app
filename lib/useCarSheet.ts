41821e7b99c5bbbecd252ab6160592ff03c58808
Mazen Essam <mazenessam2011@outlook.com>
2026-01-25 18:57:04 +0200

    make polling time to 15 sec

--- lib/useCarSheet.ts
+++ lib/useCarSheet.ts
@@ -7,7 +7,7 @@ export function useCarsSheet(sheet: string, date?: string) {
     `/api/cars/sheet/${sheet}${date ? `?date=${date}` : ""}`,
     fetcher,
     {
-      refreshInterval: 30000, // fallback polling
+      refreshInterval: 15_000, // fallback polling
       revalidateOnFocus: true,
     },
   );
