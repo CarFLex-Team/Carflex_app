906b2d9d574f7988e74cfe776f6e9a2d35b6a867
Mazen Essam <mazenessam2011@outlook.com>
2026-02-07 16:00:33 +0200

    make all listign limit to 100

--- app/(with-nav-shell)/listings/page.tsx
+++ app/(with-nav-shell)/listings/page.tsx
@@ -1,4 +1,4 @@
 import Listings from "@/components/Listings/Listings";
 export default async function All() {
-  return <Listings active="All" limit={50} />;
+  return <Listings active="All" limit={100} />;
 }
